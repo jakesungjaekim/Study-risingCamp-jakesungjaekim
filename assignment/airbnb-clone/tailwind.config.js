e6303e97ed8a07a33c8e1db4ec05d8a0629219a6
jakesungjaekim <codejakecarry@gmail.com>
2023-07-18 18:43:35 +0900

    docs: tailwindcss screens setting

--- assignment/airbnb-clone/tailwind.config.js
+++ assignment/airbnb-clone/tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      'xs': '450px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl':'1540px',
+    },
     extend: {},
   },
   plugins: [],
