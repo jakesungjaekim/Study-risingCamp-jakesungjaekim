8f0318037395a4d42ea5dda778aadde41d1ce621
jakesungjaekim <codejakecarry@gmail.com>
2023-07-19 12:34:39 +0900

    feat: carousel 반응형 구간 제어

--- assignment/airbnb-clone/src/components/home/Carousel.tsx
+++ assignment/airbnb-clone/src/components/home/Carousel.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import ImgFilter from '../../assets/images/image-filter.svg'
 
 const Carousel:React.FC = () => {
@@ -6,7 +6,7 @@ const Carousel:React.FC = () => {
   return (
     <section className='w-screen px-10 pt-5'>
       <div className='flex w-full h-[78px]'>
-        <div className='min-w-[646.23px] h-full bg-rose-50'></div>
+        <div className='w-full md:min-w-[646.23px] lg:min-w-[843.23px] xl:min-w-[1099.23px] 2xl:px-20 2xl:min-w-[1279.23px] 3xl:w-full h-full bg-rose-50 transition duration-500'></div>
         <div className='flex items-center justify-center min-w-[100.7px] h-full pl-6'>
           <button className='flex items-center justify-center w-full h-12 border border-gray-200 rounded-xl'>
             <img src={ImgFilter} alt="image-filter" />
--- assignment/airbnb-clone/tailwind.config.js
+++ assignment/airbnb-clone/tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       'lg': '1024px',
       'xl': '1280px',
       '2xl':'1540px',
+      '3xl': '1920px'
     },
     extend: {
       fontFamily: {
