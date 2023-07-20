a0fd1a6dd96509f82afe7d044de16bd651caa82f
jakesungjaekim <codejakecarry@gmail.com>
2023-07-20 19:41:42 +0900

    HOTFIX: windowSizeApi delete

--- assignment/airbnb-clone/src/store/index.ts
+++ assignment/airbnb-clone/src/store/index.ts
@@ -1,19 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit' 
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { windowSizeApi } from './apis/windowSizeApi'
 
 
 export const store = configureStore({
   reducer: {
-    [windowSizeApi.reducerPath]: windowSizeApi.reducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) => {
     return  getDefaultMiddleware()
-    .concat(windowSizeApi.middleware)
   }
 })
 
 setupListeners(store.dispatch)
-
-export { useGetWindowSizeQuery } from './apis/windowSizeApi'
